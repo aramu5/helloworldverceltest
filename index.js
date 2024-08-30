2a3ef08ecb64fff59c8b1f9ec8631497ee124a5e
Aramu5 <aramandreasyan@gmail.com>
2024-08-30 16:12:47 +0100

    1

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
-require('dotenv').config(); // This should be at the very top test
+require('dotenv').config({ path: require('path').resolve(__dirname, '.env') });
 
 const axios = require('axios');
+const cheerio = require('cheerio');
 
 const DISCORD_BOT_TOKEN = process.env.DISCORD_BOT_TOKEN;
 const CHANNEL_ID = process.env.DISCORD_CHANNEL_ID;
@@ -21,9 +22,5 @@ const sendMessageToDiscord = async () => {
         console.error('Error sending message:', error.response ? error.response.data : error.message);
     }
 };
-
-console.log('Bot Token:', DISCORD_BOT_TOKEN);
-console.log('Channel ID:', CHANNEL_ID);
-
 
 sendMessageToDiscord();
--- testscript.js
+++
@@ -1,4 +0,0 @@
-require('dotenv').config(); // Load .env file
-
-console.log('DISCORD_BOT_TOKEN:', process.env.DISCORD_BOT_TOKEN);
-console.log('DISCORD_CHANNEL_ID:', process.env.DISCORD_CHANNEL_ID);
